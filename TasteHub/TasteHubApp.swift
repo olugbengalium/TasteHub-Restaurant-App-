8c112062f705693b8e82ff132eed10a85a29d2bb
Akinbande Gbenga <olugbengalium@gmail.com>
2023-04-21 04:03:32 +0100

    Initial Commit

---
+++ TasteHub/TasteHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasteHubApp.swift
+//  TasteHub
+//
+//  Created by Gbenga Abayomi on 21/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct TasteHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
